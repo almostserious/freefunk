0df91a7c6f9e46b36f3928e18217bc9227b0165f
almostserious <mail@marcus-peters.de>
2019-07-24 21:37:45 +0200

    Add files via upload
    Added service calls to Update Status, Pause, Book 1GB & Book Unlimited Tariff

--- custom_components/freefunk/__init__.py
+++ custom_components/freefunk/__init__.py
@@ -1,4 +1,8 @@
 DOMAIN = 'freefunk'
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
 from homeassistant.const import (
     CONF_PASSWORD, CONF_USERNAME)
 def setup(hass, config):
@@ -10,10 +14,78 @@ def setup(hass, config):
     api = FunkAPI(username, password)
     currentPlan = api.getCurrentPlan()
     hass.states.set('freefunk.status', currentPlan["productServiceInfo"]["label"])
-	 
-    # paused = hass.states.get("input_boolean.paused").state == "on"
-    # if paused:
-        # status = api.startPause()
-        # hass.states.set('freefunk.error', status["errors"][0]["message"])
-    # hass.states.set('freefunk.state', paused)
+    _LOGGER.info(currentPlan)
+ 
+    def pause(call):
+        """Handle the pause service call."""
+        conf = config[DOMAIN]
+        username = conf[CONF_USERNAME]
+        password = conf[CONF_PASSWORD]
+        import funkapi
+        from funkapi import FunkAPI
+        api = FunkAPI(username, password)
+        status = api.startPause()
+        _LOGGER.info(status)
+        import time
+        time.sleep(10) 
+        currentPlan = api.getCurrentPlan()
+        hass.states.set('freefunk.status', currentPlan["productServiceInfo"]["label"])
+        _LOGGER.info(currentPlan)
+
+    hass.services.register(DOMAIN, 'pause', pause)
+
+    def update(call):
+        """Handle the update service call."""
+        conf = config[DOMAIN]
+        username = conf[CONF_USERNAME]
+        password = conf[CONF_PASSWORD]
+        import funkapi
+        from funkapi import FunkAPI
+        api = FunkAPI(username, password)
+        currentPlan = api.getCurrentPlan()
+        hass.states.set('freefunk.status', currentPlan["productServiceInfo"]["label"])
+        _LOGGER.info(currentPlan)
+
+
+    hass.services.register(DOMAIN, 'update', update)
+    
+    def book1gb(call):
+        """Handle the update service call."""
+        conf = config[DOMAIN]
+        username = conf[CONF_USERNAME]
+        password = conf[CONF_PASSWORD]
+        import funkapi
+        from funkapi import FunkAPI
+        api = FunkAPI(username, password)
+        status = api.order1GBPlan()
+        _LOGGER.info(status)		
+        import time
+        time.sleep(10) 
+        currentPlan = api.getCurrentPlan()
+        hass.states.set('freefunk.status', currentPlan["productServiceInfo"]["label"])
+        _LOGGER.info(currentPlan)
+
+
+    hass.services.register(DOMAIN, 'book1gb', book1gb)
+	
+    def bookunlimited(call):
+        """Handle the update service call."""
+        conf = config[DOMAIN]
+        username = conf[CONF_USERNAME]
+        password = conf[CONF_PASSWORD]
+        import funkapi
+        from funkapi import FunkAPI
+        api = FunkAPI(username, password)
+        status = api.orderUnlimitedPlan()
+        _LOGGER.info(status)		
+        import time
+        time.sleep(10) 
+        currentPlan = api.getCurrentPlan()
+        hass.states.set('freefunk.status', currentPlan["productServiceInfo"]["label"])
+        _LOGGER.info(currentPlan)
+
+
+    hass.services.register(DOMAIN, 'bookunlimited', bookunlimited)
+	
+	
     return True
